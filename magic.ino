f10fc58fea1ea91c4d987cd2b0c1925aa30f9b1a
Lindsey Bieda <lindseyb@github.com>
2019-02-15 18:27:55 -0500

    When everything is safe just breathe

--- magic.ino
+++ magic.ino
@@ -8,7 +8,7 @@
 
 #define PIN 14
 #define NUM_LEDS 7
-#define BRIGHTNESS 100
+#define BRIGHTNESS 255
 #define RED 0
 #define ORANGE 1
 #define YELLOW 2
@@ -22,32 +22,36 @@
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
-const char* ssid     = "SSID";
-const char* password = "password";
+const char* ssid     = "SSID_HERE";
+const char* password = "PASSWORD_HERE";
 
 const char* mercuryHost = "mercuryretrogradeapi.com";
-const char* moonHost = "isitfullmoon.com";
+const char* moonHost    = "isitfullmoon.com";
 
-const int httpPort = 80;
+const int httpPort  = 80;
 const int httpsPort = 443;
 
 const unsigned long SECOND = 1000;
-const unsigned long HOUR = 3600*SECOND;
+const unsigned long HOUR   = 3600*SECOND;
 
 // specific to the mecuryretrogradeapi.com cert 
 const char fingerprint[] PROGMEM = "4a 36 25 b9 1f 4c 6d 61 c4 6f d3 8d 65 70 21 96 b0 05 13 0f";
 
+const float MAX_BRIGHTNESS = 64;
+const float SPEED          = 0.008; 
+const int FADE_DELAY       = 5;
+
 uint32_t COLORS[] = {
-  strip.Color(255, 0, 0),    // red 
-  strip.Color(255, 165, 0),  // orange
-  strip.Color(255, 255, 0),  // yellow
-  strip.Color(0, 255, 0),    // green
-  strip.Color(0, 0, 255),    // blue
-  strip.Color(75, 0, 130),   // indigo
-  strip.Color(128, 0, 128),  // violet
-  strip.Color(255, 192, 203),// pink
-  strip.Color(255, 255, 255),// white
-  strip.Color(0, 0, 0)       // off
+  strip.Color(255, 0, 0),     // red 
+  strip.Color(255, 165, 0),   // orange
+  strip.Color(255, 255, 0),   // yellow
+  strip.Color(0, 255, 0),     // green
+  strip.Color(0, 0, 255),     // blue
+  strip.Color(135, 206, 235), // indigo
+  strip.Color(128, 0, 128),   // violet
+  strip.Color(255, 192, 203), // pink
+  strip.Color(255, 255, 255), // white
+  strip.Color(0, 0, 0)        // off
 };
 
 void setup() {
@@ -68,13 +72,19 @@ void setup() {
 void loop() {
   if (isMercuryRetrograde()) {
     set_color_to(RED);
+    delay(HOUR*12); 
   } else if (isMoonFull()) {
     set_color_to(WHITE);
+    delay(HOUR*12); 
   } else {
-    set_color_to(OFF);
+    // breathe slowly 
+    for (unsigned long i = 0; i < (HOUR*12)/FADE_DELAY; i++) {
+      float intensity = MAX_BRIGHTNESS /2.0 * (1.0 + sin(SPEED * i));
+      strip.setBrightness(intensity);
+      set_color_to(INDIGO);
+      delay(FADE_DELAY);
+    }
   }
-
-  delay(HOUR*12);  
 }
 
 bool isMoonFull() {
